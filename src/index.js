a2c749041242a7b80799e09e57ee9d750bc05737
lucaslima <lucas.m.lima6@edu.senai.br>
2026-08-26 10:46:47 -0300

    fix: refatoração das pastas
    Co-authored-by: Isabela <isabela.f.silva@edu.senai.br>
    Co-authored-by: GUILHERME TOMAZ <guilhermetomaz11@users.noreply.github.com>
    Co-authored-by: Helogigliott <heloisasoroca-ai@users.noreply.github.com>

---
+++ src/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+const PORTA = 3000;
+
+app.get("/", (req, res) => {
+  res.send("API da Livraria no ar!");
+});
+
+app.listen(PORTA, () => {
+  console.log("Servidor rodando em http://localhost:" + PORTA);
+});
+
+app.get("/sobre", (req, res) => {
+res.send("Livraria SENAI - Trabalho de PBE, turma 1-2026-SESI_DEV_OC_1");
+});
