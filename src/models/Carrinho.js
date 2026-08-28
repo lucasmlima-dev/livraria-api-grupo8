ad016f14af946b35ab4fcba29fd3dbc39471a22d
lucaslima <lucas.m.lima6@edu.senai.br>
2026-08-28 15:06:28 -0300

    feat: finaliza esqueleto MVC e audita coerencia com o diagrama UML

---
+++ src/models/Carrinho.js
@@ -0,0 +1,32 @@
+class Carrinho {
+  #livros;
+
+  constructor() {
+    this.#livros = [];
+  }
+
+  adicionarLivro(livro) {
+    this.#livros.push(livro);
+  }
+
+  removerLivro(titulo) {
+    this.#livros = this.#livros.filter((livro) => livro.titulo !== titulo);
+  }
+
+  listar() {
+    console.log("--- Itens do carrinho ---");
+    this.#livros.forEach((livro) => {
+      console.log(livro.titulo + " - R$ " + livro.preco);
+    });
+  }
+
+  calcularTotal() {
+    let total = 0;
+    this.#livros.forEach((livro) => {
+      total = total + livro.preco;
+    });
+    return total;
+  }
+}
+
+module.exports = Carrinho;
